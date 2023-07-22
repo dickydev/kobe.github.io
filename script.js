e5a6c975059c67f643cc0a4966542414ec2e2c9e
Dicky Fernando <macbookpro@macbookPros-MacBook-Pro.local>
2023-07-22 09:06:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function goToBiography()  {
+    window.location.assign("biography.html")
+}
+
+function goToAchievements()  {
+        window.location.assign("achievements.html")
+}
+
+function goToHome()  {
+        window.location.assign("home.html")
+}
+
